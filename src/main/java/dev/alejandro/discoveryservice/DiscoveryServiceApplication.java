c1a92d29be7f64bcd3a93dd855968a80f2d8ddec
alejandro <alejandroriveros100@gmail.com>
2025-01-21 23:40:22 -0500

    [fix] disabled config-server

--- src/main/java/dev/alejandro/discoveryservice/DiscoveryServiceApplication.java
+++ src/main/java/dev/alejandro/discoveryservice/DiscoveryServiceApplication.java
@@ -1,13 +1,15 @@
 package dev.alejandro.discoveryservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableConfigServer
 public class DiscoveryServiceApplication {
 
     public static void main(String[] args) {
